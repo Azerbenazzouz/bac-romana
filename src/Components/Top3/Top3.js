f8b816704b7ce8119ba7511c6f6e1bd0b3e8be51
azer <benazzouzaze@gmail.com>
2022-01-19 22:29:15 +0100

    ❤❤

--- src/Components/Top3/Top3.js
+++ src/Components/Top3/Top3.js
@@ -28,8 +28,8 @@ function Top3() {
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     )
