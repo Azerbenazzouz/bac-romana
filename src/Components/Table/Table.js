61e78caef4a5924baf0cd56d5eb20a9c335377f1
azer <benazzouzaze@gmail.com>
2022-01-21 22:27:55 +0100

    ❤❤

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,18 +1,32 @@
-import React,{useMemo} from 'react'
-import makeData from '../../data/data.json'
+import React,{useState,useEffect} from 'react'
 import "./table.css"
+import Avatar from '@mui/material/Avatar';
+import { collection , onSnapshot } from "firebase/firestore"; 
+import db from "../../Firebase/firebase"
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Table() {
-    const data = useMemo(()=>makeData,[])
+    const [data,setdata]=useState([])
     
+    useEffect(()=>{
+        onSnapshot(collection(db,"Liste des donateurs"),(snapshot)=>{
+            setdata(snapshot.docs.map(doc => doc.data()));
+        });
+    })
+
     return (
         <div>
             <table>
                 <thead>
                     <tr>
-                        <th>ID</th>
+                        <th>#</th>
+                        <th>Img</th>
                         <th>Name</th>
-                        <th>Movies</th>
+                        <th>Date</th>
+                        <th>Valeur</th>
+                        <th>réseaux sociaux</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -20,8 +34,22 @@ function Table() {
                         return (
                             <tr key={i}>
                                 <td>{i+1}</td>
-                                <td>{req.Name}</td>
-                                <td>{req.Movies}</td>
+                                <td><Avatar alt={req.Nom} src={req.img} /></td>
+                                <td>{req.Nom}</td>
+                                <td>{req.date}</td>
+                                <td>{req.value} TND</td>
+                                <td>
+                                    {
+                                        req.sociaux.facebook==="" ? <span></span>:(
+                                            <a target="_blank" href={req.sociaux.facebook}><FacebookIcon sx={{ fontSize: 30 }} style={{color:"#3b5998"}}/></a>
+                                        )
+                                    }
+                                    {
+                                        req.sociaux.instagram==="" ? <span></span>:(
+                                            <a target="_blank" href={req.sociaux.instagram}><InstagramIcon sx={{ fontSize: 30 }} style={{color:"#c13584"}}/></a>
+                                        )
+                                    }
+                                </td>
                             </tr>
                         )
                     })}
--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,12 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
-
-
-
-const firebaseConfig = {
+// Initialize Firebase
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyD_whQf2Oh_E-MdwtFsDDW94GG4ruK09z0",
   authDomain: "bac-info-2022.firebaseapp.com",
   projectId: "bac-info-2022",
@@ -14,8 +12,6 @@ const firebaseConfig = {
   messagingSenderId: "600387437241",
   appId: "1:600387437241:web:35e639a69ce4a8dae81168",
   measurementId: "G-Z96MH8C026"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export default getFirestore();
